350059216f786ff817e0e943ce84d7452edcf47e
Alisina Haidari <alisinahaidari1382@gmail.com>
2026-01-09 11:14:46 +0430

    added query parameters along with its validations

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,25 +3,32 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sort) { // when we set the name to "sort" then it no matter what the variable name is we can set it to sortBy
+        if(!Set.of("name","email").contains(sort)){
+            sort = "name";
+        }
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
-                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList();
+//                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList(); // OLD WAY
+//                .map(user -> userMapper.toDto(user)).toList(); // LONG WAY
+                .map(userMapper::toDto).toList(); // SHORT WAY
     }
 
     @GetMapping("/{id}")
@@ -30,7 +37,8 @@ public class UserController {
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
-        return ResponseEntity.ok(userDto);
+//        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail()); // NO NEED ANYMORE
+//        return ResponseEntity.ok(userDto);
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 }
