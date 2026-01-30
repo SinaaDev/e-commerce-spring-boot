9abbde62390dc283af952ee53abb6e6b7615ac7c
Alisina Haidari <alisinahaidari1382@gmail.com>
2026-01-30 22:08:01 +0430

    new features added

---
+++ src/main/java/com/codewithmosh/store/services/UserService.java
@@ -0,0 +1,24 @@
+package com.codewithmosh.store.services;
+
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email);
+
+        return new User(user.getEmail(),user.getPassword(), Collections.emptyList());
+    }
+}
